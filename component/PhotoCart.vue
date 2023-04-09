cf0aa254c537927fe71d6ede2ea11bd98cb18029
YihaoCCC <yhao88@126.com>
2023-04-09 23:53:53 +0800

    🌈calendar页面样式更改

--- component/PhotoCart.vue
+++ component/PhotoCart.vue
@@ -1,25 +1,5 @@
 <template>
-	<view class="calendar-card" v-if="calendar">
-		<image class="card-img" src="https://cdn.zebraui.com/zebra-ui/images/swipe-demo/swipe1.jpg" mode="aspectFill" @load="onSuccessImg" v-show='isImageLoaded'></image>
-		<view class="card-img loading" v-show='!isImageLoaded'>
-			<uni-load-more status="loading" :content-text="{contentrefresh: '加载中'}" />
-		</view>
-		<view class="model">
-			<view class="model-title">
-				Madinah Mosque Nabawi
-			</view>
-			<view class="model-date">
-				Thursday, July 28th to July 31st
-			</view>
-			<view class="guide">
-				<view class="arrow arrow-1"></view>
-				<view class="arrow arrow-2" ></view>
-				<view class="arrow arrow-3" ></view>
-			</view>
-		</view>
-		
-	</view>
-	<view v-else :class="crossExpandId === imgObj._id ? 'active':''">
+	<view :class="crossExpandId === imgObj._id ? 'active':''">
 		<scroll-view class="card-box" :class="imgObj.imgType === 'vertical' ? 'vertical' : 'cross' " @click="handleClick">
 			<image class="card-img" v-show="isImageLoaded" :src="imgObj.url" mode="aspectFill"  @load="onSuccessImg" :lazy-load="true"></image>
 			<view class="card-img loading" v-show='!isImageLoaded'>
@@ -156,86 +136,5 @@
 				transition: all .3s cubic-bezier(0,1,0.95,1.05); 
 			}
 		}	
-	}
-	.calendar-card {
-		width: 100%;
-		height: 220px;
-		border-radius: 16px;
-		overflow: hidden;
-		position: relative;
-		margin-bottom: 10px;
-		.model {
-			border-radius: 16px;
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			background-color:  rgb(3, 31, 42, 0.3);
-			color: #fff;
-			display: flex;
-			align-items: center;
-			flex-direction: column;
-			justify-content: center;
-			font-family: cursive;
-			.guide {
-				transform: rotate(-90deg);
-				position: absolute;
-				top: 16%;
-				right: 18%;
-			}
-			.arrow:after, .arrow:before {
-				background: #fff;
-				content: "";
-				display: block;
-				height: 2px;
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 9px;
-				box-shadow: 1px 1px 20px 0 #fff;
-			}
-			.arrow:before {
-				transform: rotate(45deg) translateX(-11%);
-				transform-origin: top left;
-			}
-			.arrow:after {
-				transform: rotate(-45deg) translateX(9%);
-				transform-origin: top right;
-			}
-			.arrow-1 {
-				animation: arrow-movement 3s ease-in-out infinite;
-			}
-			.arrow-2 {
-				animation: arrow-movement 3s 1s ease-in-out infinite;
-			}
-			.arrow-3 {
-				animation: arrow-movement 3s 2s ease-in-out infinite;
-			}
-			.arrow {
-				position: absolute;
-				top: 35px;
-				transform-origin: 50% 50%;
-				transform: translate3d(-50%,0,0);
-			}
-			@keyframes arrow-movement {
-			    0% {
-			        opacity: 0;
-			        top: 0;
-			    }
-			    70% {
-			        opacity: 1;
-			    }
-			    100% {
-			        opacity: 0;
-			    }
-			}
-			.model-title {
-				font-weight: 600;
-				font-size: 18px;
-				margin-bottom: 5px;
-			}
-		}
-		
 	}
 </style>
--- pages/calendar/calendar.vue
+++ pages/calendar/calendar.vue
@@ -1,46 +1,61 @@
 <template>
-	<view class="calendar">
-		<view class="calendar-title">
-			<uni-icons type="calendar" size="32" color="var(--theme-color)"></uni-icons>
-			
-			Trip Plan
-		</view>
+	<view class="calendar" :style="{overflow: disabledScroll ? 'hidden': 'auto'}">
 		
-		<scroll-view class="calendar-content" :scroll-y="true">
-			<view class="calendar-date">
-				2023年 3月 
+		<view class="calendar-card" :style="transform" :class="active === index ? 'active' : ''" v-for="(item,index) in 5"
+			:key="index" @click="(e) => activeCard(index, e)">
+			<image class="card-img" src="https://cdn.zebraui.com/zebra-ui/images/swipe-demo/swipe2.jpg"
+				mode="scaleToFill"></image>
+				
+			<view class="date">
+				
+				<uni-icons type="calendar" size="30" color="#fff"></uni-icons>
+				<view class="time">
+					<text>Thursday, July 28th to July 31st</text>
+				</view>
 			</view>
-			<view class="calendar-item" v-for="(item,index) in 5" :key="index">
-				<photo-card  :calendar='true'></photo-card>
+			<view class="close" @click.stop="close">
+				<uni-icons type="closeempty" size="20" color="#fff"></uni-icons>
 			</view>
-			
-		</scroll-view>
+			<view class="hidden-content">
+				
+			</view>
+		</view>
 	</view>
 </template>
 
 <script setup>
-	import { onLoad, onShow } from "@dcloudio/uni-app";
-	import { ref, computed, reactive, onMounted } from 'vue';
-	import PhotoCard from '../../component/PhotoCart.vue'
-	onMounted(() => {
-		
-	})
-	const list2 = ref([{
-					title: '买家下单',
-					desc: '2018-11-11'
-				}, {
-					title: '卖家发货',
-					desc: '2018-11-12'
-				}, {
-					title: '买家签收',
-					desc: '2018-11-13'
-				}, {
-					title: '交易完成',
-					desc: '2018-11-14'
-				}]) 
-	const active  = ref(2)
+	import {
+		onLoad,
+		onShow,
+		onPageScroll
+	} from "@dcloudio/uni-app";
+	import {
+		ref,
+		computed,
+		reactive
+	} from 'vue';
+
+	const active = ref(1000)
+	const transform = ref('')
+	const disabledScroll = ref(false)
+	const activeCard = (index, e) => {
+		disabledScroll.value = true
+		console.log(window);
+		// 盒子只触发一次点击事件
+		if(active.value !== index) {
+			active.value = index
+			let selectorQuery = uni.createSelectorQuery();
+			selectorQuery.selectAll('.calendar-card').boundingClientRect((rects) => {
+				console.log(rects);
+				transform.value = `--move-height: -${rects[index].top}px`
+			}).exec()
+		}
+	}
+	const close = () => {
+		active.value = 999
+	}
 </script>
 
-<style lang="scss" scoped> 
-	@import './calendar.scss';
+<style lang="scss" scoped>
+ @import './calendar.scss';
 </style>
